fdbb034d39d26f7b29beb23a6431ae4467f2871b
JangHyuk00 <yim0823@hotmail.com>
2021-03-05 22:22:19 +0900

    middlewares with morgan&helmet&bodyParser

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000
@@ -9,12 +13,12 @@ const handleHome = (req,res) => res.send("Hello from Home!");
 
 const handleProfile = (req,res) => res.send("You are on my profile");
 
-const middleHome = (req,res,next) => {
-    console.log("Between");
-    next();
-}
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(helmet());
 
-app.use(middleHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
