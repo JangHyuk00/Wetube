818c6bd9766c40b956a294eeeeb38a39d427d471
JangHyuk00 <yim0823@hotmail.com>
2021-03-06 23:10:52 +0900

    home controller with fake DB(1)

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:23982,
+        title: "Video Awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creater: {
+            id: 121212,
+            name: "Jimmy",
+            email: "ren@las.com"
+        }
+    },
+    {
+        id:23232,
+        title: "Video Super",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creater: {
+            id: 121212,
+            name: "Jimmy",
+            email: "ren@las.com"
+        }
+    },
+    {
+        id:23982,
+        title: "Video Nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creater: {
+            id: 121212,
+            name: "Jimmy",
+            email: "ren@las.com"
+        }
+    },
+    {
+        id:23982,
+        title: "Video Perfect",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creater: {
+            id: 121212,
+            name: "Jimmy",
+            email: "ren@las.com"
+        }
+    }
+]
