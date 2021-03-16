db49dc6a334c49f28808719a4cc229c329c3d30d
JangHyuk00 <yim0823@hotmail.com>
2021-03-16 23:57:08 +0900

    ES6 with Webpack and connect css with app

--- webpack.config.js
+++ webpack.config.js
@@ -7,51 +7,59 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-entry: ENTRY_FILE,
+entry: ["@babel/polyfill", ENTRY_FILE],
 mode: MODE,
 module: {
-rules: [
-{
-test: /\.(scss)$/,
-use: [
-    {
-        loader: MiniCssExtractPlugin.loader
-    },
-    {
-        loader: "css-loader"
-    },
-    {
-        loader: "postcss-loader",
-        options: {
-                postcssOptions: {
-                    plugins: [
-                        [
-                            'autoprefixer',
-                            {
-                                overrideBrowserslist: "cover 99.5%"
-                            },
-                        ]
-                    ]
+    rules: [
+        {
+            test: /\.(js)$/,
+            use: [
+                {
+                    loader: "babel-loader"
                 }
-            }
-    },
-    {
-        loader: "sass-loader"
-    }
-]
-}
-]
+            ]
+        },
+        {
+            test: /\.(scss)$/,
+            use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                                postcssOptions: {
+                                    plugins: [
+                                        [
+                                            'autoprefixer',
+                                            {
+                                                overrideBrowserslist: "cover 99.5%"
+                                            },
+                                        ]
+                                    ]
+                                }
+                            }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+        ]
+        }
+    ]
 },
 output: {
-path: OUTPUT_DIR,
-filename: "[name].js"
+    path: OUTPUT_DIR,
+    filename: "[name].js"
 },
 plugins: [
-new MiniCssExtractPlugin({
-// Options similar to the same options in webpackOptions.output
-// both options are optional
-filename: 'style.css'
-}),
+    new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: 'style.css'
+    }),
 ]
 };
 
