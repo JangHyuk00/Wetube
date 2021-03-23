f1c2b4e74b5d27c7d3b0c1c8a2d1040ba14441dd
JangHyuk00 <yim0823@hotmail.com>
2021-03-23 22:15:21 +0900

    register user by "user" & password with serializeUser

--- passport.js
+++ passport.js
@@ -3,3 +3,7 @@ import User from "./models/User";
 
 // use means use a strategy from passport
 passport.use(User.createStrategy());
+
+// "hey passport, only send the user id as cookie"
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
