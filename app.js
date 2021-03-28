a443be5f2b3c118a0cc43c753eff01e125a6c48f
JangHyuk00 <yim0823@hotmail.com>
2021-03-28 19:17:06 +0900

    express session (cannot recognize req.user)

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cookieParser from "cookie-parser";
 import passport from "passport";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 import bodyParser, { urlencoded } from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -19,13 +20,18 @@ app.use(helmet({
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: true,
+  saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cookieParser());
 
 
 app.use(routes.home, globalRouter);
