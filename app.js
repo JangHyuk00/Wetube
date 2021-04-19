e61ec72ac028a36a41bf2258c8b1a91d7d94ba81
JangHyuk00 <yim0823@hotmail.com>
2021-04-19 20:04:03 +0900

    api register a view (1)

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import bodyParser, { urlencoded } from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import apiRouter from "./routers/apiRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
 import "./passport";
@@ -41,5 +42,6 @@ app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+app.use(routes.api, apiRouter);
 
 export default app;
